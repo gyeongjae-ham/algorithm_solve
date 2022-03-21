4e7f94bf265eb9647201eb2f3c6f34ca3e95b02f
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-03-21 22:42:51 +0900

    :sparkles: Add 2557, 10718, 1000, 10172, 10171

---
+++ boj/입출력과사칙연산-c++/A+B_1000.cpp
@@ -0,0 +1,22 @@
+//
+// Created by 함경재 on 2022/03/21.
+//
+
+//문제
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//
+//입력
+//첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//
+//출력
+//첫째 줄에 A+B를 출력한다.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int a,b;
+int main(){
+    cin >> a >> b;
+    cout << a+b;
+    return 0;
+}
