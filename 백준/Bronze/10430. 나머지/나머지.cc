4b3ce170e01e5c7e432a3bce576be01d19d2c0db
Gyeongjae Ham <75306311+hiyee-gj@users.noreply.github.com>
2022-03-23 17:42:41 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int A,B,C;
+int main(){
+    cin>>A>>B>>C;
+    cout<<(A+B)%C<<endl;
+    cout<<((A%C)+(B%C))%C<<endl;
+    cout<<(A*B)%C<<endl;
+    cout<<((A%C)*(B%C))%C;
+    return 0;
+}
