f6bb045e370730fe49468cfbb99bd65dd690d755
Gyeongjae Ham <75306311+hiyee-gj@users.noreply.github.com>
2022-03-28 17:54:56 +0900

    [Bronze IV] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int x,y;
+
+int main(){
+    cin>>x>>y;
+    if(x>0 && y>0){
+        cout<<1;
+    }
+    else if(x<0 && y>0){
+        cout<<2;
+    }
+    else if(x<0 && y<0){
+        cout<<3;
+    }
+    else {
+        cout<<4;
+    }
+    
+    return 0;
+}
