ce738a5a840cb08f755f45f0b2ffb1d45dc83398
Gyeongjae Ham <75306311+hiyee-gj@users.noreply.github.com>
2022-03-30 09:37:08 +0900

    [Bronze III] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N,a,b,i;
+
+int main(){
+    cin>>N;
+    for(i=0; i<N; i++){
+        cin>>a>>b;
+        cout<<a+b<<"\n";
+    }
+    return 0;
+}
