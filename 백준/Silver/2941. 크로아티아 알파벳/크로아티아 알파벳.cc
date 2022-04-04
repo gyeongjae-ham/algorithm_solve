a64aefa771a6240184284c0c879aece22c7b4492
Gyeongjae Ham <75306311+hiyee-gj@users.noreply.github.com>
2022-04-04 16:14:05 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -2,11 +2,10 @@
 using namespace std;
 vector<string> cr = {"c=","c-","dz=","d-","lj","nj","s=","z="};
 string s;
-typedef long long int ll;
 
 int main() {
     cin>>s;
-    for(ll i=0; i<cr.size(); i++) {
+    for(int i=0; i<cr.size(); i++) {
         while(true) {
             int idx = s.find(cr[i]);
             
