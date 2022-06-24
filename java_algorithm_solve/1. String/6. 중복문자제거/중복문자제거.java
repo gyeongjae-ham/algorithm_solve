cb74a737572c47820ef043fdec8a2bc361fd17a4
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-06-24 18:14:33 +0900

    :sparkles: Add java_algoritm_solve string - 6. 중복문자제거

---
+++ java_algorithm_solve/1. String/6. 중복문자제거/중복문자제거.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static String solution(String str) {
+        String answer = "";
+        for (int i=0; i<str.length(); i++) {
+            // ? 문자가 발견된 위치가 지금 인덱스 위치랑 같으면 최초로 발견된 문자고 다르다면 중복된 문자다!!
+            if (str.indexOf(str.charAt(i)) == i) answer += str.charAt(i);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        System.out.print(solution(word));
+    }
+}
