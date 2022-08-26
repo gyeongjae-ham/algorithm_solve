ed47887275d51644981c26f5ed078350b935a79c
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2022-08-26 16:37:28 +0900

    [Silver IV] Title: 보물, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+res=0
+for i in range(n):
+    res += (a[i]*b[i])
+
+print(res)
