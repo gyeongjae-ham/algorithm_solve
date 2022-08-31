20bcfc7d4edaefddd2c4e0ee3eaf9c89b45bc39b
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2022-08-31 18:00:08 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 628 ms, Memory: 156172 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n, m, start = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+
+for _ in range(m):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+res = 1
+def dfs(start):
+    global res
+    visited[start] = res
+    graph[start].sort()
+
+    for i in graph[start]:
+        if visited[i] == 0:
+            res += 1
+            dfs(i)
+
+dfs(start)
+
+for i in range(1, n+1):
+    print(visited[i])
