6edc28e799aeb962e8e137a25c4764f37c47acf7
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2022-09-05 15:31:58 +0900

    [Silver IV] Title: 덱, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    deque<int> dq;
+    int n;
+    cin>>n;
+    while(n--) {
+        string s;
+        cin>>s;
+        if(s=="push_back"){
+            int num;
+            cin>>num;
+            dq.push_back(num);
+        }
+        else if(s=="push_front"){
+            int num;
+            cin>>num;
+            dq.push_front(num);
+        }
+        else if(s=="pop_front"){
+            if(dq.empty()){
+                cout<<-1<<"\n";
+            }
+            else {
+                cout<<dq.front()<<"\n";
+                dq.pop_front();
+            }
+        }
+        else if(s=="pop_back"){
+            if(dq.empty()){
+                cout<<-1<<"\n";
+            }
+            else {
+                cout<<dq.back()<<"\n";
+                dq.pop_back();
+            }
+        }
+        else if(s=="size"){
+            cout<<dq.size()<<"\n";
+        }
+        else if(s=="empty"){
+            cout<<dq.empty()<<"\n";
+        }
+        else if(s=="front"){
+            if(dq.empty()){
+                cout<<-1<<"\n";
+            }
+            else {
+                cout<<dq.front()<<"\n";
+            }
+        }
+        else {
+            if(dq.empty()){
+                cout<<-1<<"\n";
+            }
+            else{
+                cout<<dq.back()<<"\n";
+            }
+        }
+    }
+    
+    return 0;
+}
