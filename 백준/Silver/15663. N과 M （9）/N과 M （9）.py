315cf80be97e24247ccd7e08443a9ad97613eba1
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2022-09-15 13:29:01 +0900

    [Silver II] Title: N과 M (9), Time: 132 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,26 @@
+import sys
+input=sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def func():
+    if len(res)==m:
+        print(*res)
+        return
+    
+    pre = 0
+    for i in range(n):
+        if vist[i]: continue
+        if pre == s[i]: continue
+        
+        res.append(s[i])
+        vist[i] = True
+        pre = s[i]
+        func()
+        res.pop()
+        vist[i] = False
+
+n,m = map(int, input().split())
+s = sorted(list(map(int, input().split())))
+res = []
+vist = [False] * n
+func()
