5da7057790382ad1bd2aaffca6d699f727daf0bc
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2022-09-28 09:03:28 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+
+int t;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int a,b;
+        cin>>a>>b;
+        cout<<a+b<<"\n";
+    }
+    
+    return 0;
+}
