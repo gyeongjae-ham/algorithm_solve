ef059b89adcafed9f3f3cedf06c2bce12f1343fa
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2022-10-10 15:17:45 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    string s;
+    
+    while(getline(cin,s)) {
+        cout<<s<<"\n";
+    }
+}
