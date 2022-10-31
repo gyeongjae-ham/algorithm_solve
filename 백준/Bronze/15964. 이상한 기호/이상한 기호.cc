68d91e96325574d21d6f555a9c3b582d00b0ba1c
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2022-10-31 10:03:53 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long a,b;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin>>a>>b;
+    
+    cout<<(a+b)*(a-b)<<"\n";
+    
+    return 0;
+}
