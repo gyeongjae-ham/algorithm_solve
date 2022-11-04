8ab9f22ea37a13be2d85aaa5b704b81b793ccd7a
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2022-11-04 09:30:22 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    while(getline(cin,s)){
+        cout<<s<<"\n";
+    }
+    return 0;
+}
