6b58e742612a3cee90dbaf4bcbdd00beab873db4
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-02-28 22:38:30 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cc
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -1,20 +1,18 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-vector<int> v;
-int n, num, x, i, cnt=0;
 
-int main() {
-    ios::sync_with_stdio(0), cin.tie(0);
+int n, v, num;
+vector<int> nums;
+
+int main(void){
     cin>>n;
-    for(i=0;i<n;i++){
+    for(int i=0;i<n;i++){
         cin>>num;
-        v.push_back(num);
+        nums.push_back(num);
     }
-    cin>>x;
-    for(auto i:v){
-        if (i==x) cnt++;
-    }
-    cout<<cnt<<"\n";
+    cin>>v;
+    int cnt = count(nums.begin(), nums.end(), v);
+    cout<<cnt;
     return 0;
 }
