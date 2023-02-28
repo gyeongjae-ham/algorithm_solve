73f00dacdb9cd6bbcd6f082f139cc9afe43a7492
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-02-28 17:35:57 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-string n;
-int a[11];
 
-int main() {
-    ios::sync_with_stdio(0), cin.tie(0);
+int a[10];
+string s;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     
-    cin>>n;
-    for (auto i : n) {
-        a[i-'0']++;
+    cin>>s;
+    for(auto c:s){
+        a[c-'0']++;
     }
+    a[6]=a[9]=(a[6]+a[9]+1) / 2;
     
-    int sixnine = (a[6]+a[9]+1)/2;
-    a[6]=a[9]=sixnine;
-    
-    int max = *max_element(a, a+11);
-    
-    cout<<max<<"\n";
+    int mn = *max_element(a, a+10);
+    cout<<mn<<'\n';
     
     return 0;
 }
