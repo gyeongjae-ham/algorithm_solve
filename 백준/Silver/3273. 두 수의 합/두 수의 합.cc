5857637a55a2d6201265ea9ae932d4e091d1be9c
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-02-28 22:26:07 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,32 +1,22 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-vector<int> v;
-vector<int> res;
-int n, num, x, i;
 
-int main() {
-    ios::sync_with_stdio(0), cin.tie(0);
-    cin>>n;
-    for (i=0;i<n;i++) {
-        cin>>num;
-        v.push_back(num);
-    }
-    cin>>x;
-    
-    int start=0, end=n-1, cnt=0;
-    sort(v.begin(), v.end());
-    while (start < end) {
-        if(v[start]+v[end]==x) {
-            cnt++;
-            end--;
-        } else if(v[start]+v[end]>x){
-            end--;
-        } else {
-            start++;
-        }
-    }
-    cout<<cnt<<"\n";
+int a[2000001]={};
 
-    return 0;
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, t, x, ans=0;
+  cin >> n;
+  for (int i=0; i<n; ++i) {
+    cin >> t;
+    a[t]++;
+  }
+
+  cin >> x;
+  for (int i = 1; i < (x+1)/2; ++i) {
+    if (a[i]==1 && a[x-i]==1) ans++;
+  }
+  cout << ans;
 }
