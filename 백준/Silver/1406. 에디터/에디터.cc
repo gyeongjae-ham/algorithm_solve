1f25cc3df71019cc56baad239fecca76b60f530e
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-03-08 13:04:30 +0900

    [Silver II] Title: 에디터, Time: 40 ms, Memory: 11088 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,40 +1,71 @@
+// http://boj.kr/5c97cb6a88324537a722e8de9169e2ab
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+const int MX = 1000005;
+char dat[MX];
+int pre[MX];
+int nxt[MX];
+int unused = 1;
+
+void insert(int addr, char num){
+    dat[unused] = num;
+    pre[unused] = addr;
+    nxt[unused] = nxt[addr];
+    if(nxt[addr] != -1) pre[nxt[addr]] = unused;
+    nxt[addr] = unused;
+    unused++;
+}
+
+void erase(int addr){
+    nxt[pre[addr]] = nxt[addr];
+    if(nxt[addr] != -1) pre[nxt[addr]] = pre[addr];
+}
+
+void traversal(){
+    int cur = nxt[0];
+    while(cur!=-1){
+        cout<<dat[cur];
+        cur=nxt[cur];
+    }
+}
+
+int main(void){
     ios::sync_with_stdio(0), cin.tie(0);
-    list<char> L;
-    
+    fill(pre, pre+MX, -1);
+    fill(nxt, nxt+MX, -1);
     string s;
     cin>>s;
-    for(auto c:s) L.push_back(c);
-    auto cursor = L.end();
+    int cursor=0;
+    for(auto c:s){
+        insert(cursor, c);
+        cursor++;
+    }
+    
     int n;
     cin>>n;
-    
     while(n--){
         char nc;
         cin>>nc;
-        
-        if(nc=='P') {
+        if(nc=='P'){
             char add;
             cin>>add;
-            L.insert(cursor, add);
+            insert(cursor, add);
+            cursor = nxt[cursor];
         }
         else if(nc=='L'){
-            if(cursor!=L.begin()) cursor--;
+            if(pre[cursor]!=-1) cursor = pre[cursor];
         }
         else if(nc=='D'){
-            if(cursor!=L.end()) cursor++;
+            if(nxt[cursor]!=-1) cursor = nxt[cursor];
         }
         else{
-            if(cursor!=L.begin()){
-                cursor--;
-                cursor = L.erase(cursor);
+            if(pre[cursor]!=-1){
+                erase(cursor);
+                cursor = pre[cursor];
             }
         }
     }
-    
-    for(auto i:L) cout<<i;
+    traversal();
     return 0;
 }
