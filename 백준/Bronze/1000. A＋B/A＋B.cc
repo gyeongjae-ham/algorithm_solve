bb397d9437f552b8a39a80e2185ddc8ab7a9558e
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-03-08 21:27:31 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,15 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define X first
-#define Y second
 
-int a,b;
+int a, b;
 
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    ios::sync_with_stdio(0), cin.tie(0);
     
     cin>>a>>b;
-    cout<<a+b<<"\n";
+    cout<<a+b;
     return 0;
 }
