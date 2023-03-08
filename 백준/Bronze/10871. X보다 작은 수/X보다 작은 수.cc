77742ac0a583692e48159fedd53b5243b7ce37d4
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-03-08 21:25:53 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int n,x,a;
+int n, x, num;
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main(void) {
+    ios::sync_with_stdio(0), cin.tie(0);
     
     cin>>n>>x;
-    while(n--){
-        cin>>a;
-        if(a<x) cout<<a<<" ";
-    }
     
-    return 0;
+    for(int i=0;i<n;i++) {
+        cin>>num;
+        if(num < x) {
+            cout<<num<<' ';
+        }
+    }
 }
