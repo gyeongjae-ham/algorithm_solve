7f5523407d35433de96796d8598b58365e22a250
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-03-09 12:42:16 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[3];
+
+int main(void) {
+    ios::sync_with_stdio(0), cin.tie(0);
+    
+    for(int i=0;i<3;i++){
+        cin>>a[i];
+    }
+    sort(a, a+3);
+    
+    for(auto i:a) cout<<i<<' ';
+    
+    return 0;
+}
