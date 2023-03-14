bfe9cce378495746a0129921c890e4767e692080
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-03-14 23:09:13 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -4,13 +4,8 @@ sys.setrecursionlimit(10**6)
 
 sc = int(input())
 
-if sc > 89:
-    print("A")
-elif sc > 79:
-    print("B")
-elif sc > 69:
-    print("C")
-elif sc > 59:
-    print("D")
-else:
-    print("F")
+if sc > 89: print("A")
+elif sc > 79: print("B")
+elif sc > 69: print("C")
+elif sc > 59: print("D")
+else: print("F")
