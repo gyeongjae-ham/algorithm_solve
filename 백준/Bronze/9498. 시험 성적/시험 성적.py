edef612aed6761d32821272a50a8cc233d287b63
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-03-14 23:08:38 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+sc = int(input())
+
+if sc > 89:
+    print("A")
+elif sc > 79:
+    print("B")
+elif sc > 69:
+    print("C")
+elif sc > 59:
+    print("D")
+else:
+    print("F")
