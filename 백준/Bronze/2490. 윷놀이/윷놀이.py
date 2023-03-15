eec8b44ed2f5866a4512e4ea07633e69d2ab284c
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-03-15 15:02:17 +0900

    [Bronze III] Title: 윷놀이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.py
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -2,14 +2,8 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
-for i in range(3):
-    arr = list(map(int, input().split()))
-    ans = 0
-    for k in arr:
-        if k==0: ans+=1
-    
-    if ans == 1: print("A")
-    elif ans == 2: print("B")
-    elif ans == 3: print("C")
-    elif ans == 4: print("D")
-    else: print("E")
+yt = ['D', 'C', 'B', 'A', 'E']
+
+for _ in range(3):
+    s = sum(list(map(int, input().split())))
+    print(yt[s])
