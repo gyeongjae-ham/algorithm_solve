363f140aa0fd824b2ab9c6571c69fc4ddac8810a
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-03-17 23:09:45 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+double a,b;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    cin>>a>>b;
+    cout.precision(10);
+    cout<<a/b;
+    return 0;
+}
