b83d653405606249f513c958ee6d179a81ce43b7
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-03-25 22:56:23 +0900

    [Bronze IV] Title: 세수정렬, Time: 132 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < 3; i++) {
+            bw.write(String.valueOf(arr[i])+" ");
+        }
+        
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
