43ca3bed685596e0218e18b3d5d5afc275b1964a
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-04-02 18:35:28 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -6,36 +6,17 @@ using namespace std;
 #define Y second
 
 int n,m;
-int arr[10];
-bool isused[10];
-
-void func(int k) {
-  // m개의 수를 다 정했으면 출력
-  if(k==m) {
-    for(int i=0;i<m;i++) {
-      cout<<arr[i]<<' ';
-    }
-    cout<<'\n';
-    return;
-  }
-  
-  // k==0 일 경우 1부터
-  int st = 1;
-  // k!=0 일 경우 정해진 숫자보다 큰 수부터 시작
-  if(k!=0) st = arr[k-1] + 1;
-  for(int i=st;i<=n;i++) {
-    if(!isused[i]) {
-      arr[k] = i;
-      isused[i] = 1;
-      func(k+1);
-      isused[i] = 0;
-    }
-  }
-}
+vector<int> a;
 
 int main(void) {
   fastio;
   cin>>n>>m;
-  func(0);
+  for(int i=0;i<n;i++) a.push_back(i < m ? 0 : 1);
+  do {
+    for(int i=0;i<n;i++) {
+      if(a[i]==0) cout<< i+1 << ' ';
+    }
+    cout<<'\n';
+  } while (next_permutation(a.begin(), a.end()));
   return 0;
 }
