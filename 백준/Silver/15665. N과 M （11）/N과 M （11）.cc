c7f5d0256eba85162469f95165d9b77a967a841e
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-04-03 14:11:21 +0900

    [Silver II] Title: N과 M (11), Time: 124 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+#define X first
+#define Y second
+
+int n,m;
+int arr[10];
+int num[10];
+// bool isused[10];
+
+void func(int k) {
+  if(k==m) {
+    for(int i=0;i<m;i++) {
+      cout<<arr[i]<<' ';
+    }
+    cout<<'\n';
+    return;
+  }
+  
+  int tmp = 0;
+  for(int i=0;i<n;i++) {
+    if(tmp != num[i]){
+      arr[k] = num[i];
+      tmp = num[i];
+      func(k + 1);
+    }
+  }
+}
+
+int main(void) {
+  fastio;
+  cin>>n>>m;
+  for(int i=0;i<n;i++) cin>>num[i];
+  sort(num, num+n);
+  func(0);
+  return 0;
+}
