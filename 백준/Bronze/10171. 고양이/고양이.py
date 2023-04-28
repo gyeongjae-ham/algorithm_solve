bccbc62e3e15aabb34520cb8b27213ac32158c1c
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-04-28 11:21:50 +0900

    [Bronze V] Title: 고양이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.py
@@ -0,0 +1,11 @@
+import sys
+r = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def solution():
+    print("\\    /\\")
+    print(" )  ( ')")
+    print("(  /  )")
+    print(" \\(__)|")
+    
+solution()
