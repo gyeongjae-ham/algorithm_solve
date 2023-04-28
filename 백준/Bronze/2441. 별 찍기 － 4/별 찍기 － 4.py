470f409047483046282bc2c921ddf33ffeb3850a
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-04-28 15:45:18 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.py
@@ -0,0 +1,9 @@
+import sys
+r = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def solution():
+    n = int(r().rstrip())
+    for i in range(n,0,-1):
+        print(' '*(n-i) + '*'*i)
+solution()
