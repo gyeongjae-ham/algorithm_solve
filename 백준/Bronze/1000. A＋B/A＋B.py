d25c147a49fe864aa50bf7f0aae82b569e13fd9a
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-05-02 14:00:40 +0900

    :fire: Remove python codes

--- 백준/Bronze/1000. A＋B/A＋B.py
+++
@@ -1,9 +0,0 @@
-import sys
-r = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-
-def solution():
-    a, b = map(int, r().split())
-    print(a+b)
-    
-solution()
--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++
@@ -1,5 +0,0 @@
-def solution(arr, queries):
-    for c in queries:
-        arr[c[0]], arr[c[1]] = arr[c[1]], arr[c[0]]
-    
-    return arr
