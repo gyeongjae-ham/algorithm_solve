2710c167541ad269bf08600387f8843f0caf3b42
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-05-02 22:45:21 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 13972 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -4,12 +4,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        bw.write("Hello World!");
-        
-        bw.flush();
-        br.close();
-        bw.close();
+        System.out.println("Hello World!");   
     }
 }
