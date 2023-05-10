3933a06fd478478cf660e13a01082a48dc1acd3d
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2023-05-10 16:01:38 +0900

    [Silver III] Title: N과 M (5), Time: 228 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,30 @@
+import sys
+fi = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def solution():
+    global n, m, arr, res, isused
+    n, m = map(int, fi().split())
+    arr = list(map(int, fi().split()))
+    res = [0 for _ in range(n)]
+    isused = [False for _ in range(10000)]
+    arr.sort()
+    
+    func(0)
+
+def func(k):
+    if k == m:
+        for i in range(m):
+            print(res[i], end=" ")
+        print()
+        return
+    
+    for v in arr:
+        if not isused[v]:
+            res[k] = v
+            isused[v] = 1
+            func(k + 1)
+            isused[v] = 0
+                
+if __name__ == '__main__':
+    solution()
