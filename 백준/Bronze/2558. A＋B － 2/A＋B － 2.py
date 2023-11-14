bafc788fbf222432ccba880be5b484c2011cc2cb
luca <gyeongjae.h.dev@gmail.com>
2023-11-14 22:17:54 +0900

    [Bronze V] Title: A+B - 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.py
@@ -0,0 +1,10 @@
+import sys
+fi = sys.stdin.readline
+                
+def solution():
+    A = int(fi().rstrip())
+    B = int(fi().rstrip())
+    print(A + B)
+    
+if __name__ == '__main__':
+    solution()
