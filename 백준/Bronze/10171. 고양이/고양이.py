df5a832e3ece65b04d74a82f974eb402f081970a
luca <gyeongjae.h.dev@gmail.com>
2023-11-17 15:40:20 +0900

    [Bronze V] Title: 고양이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.py
@@ -0,0 +1,11 @@
+import sys
+fi = sys.stdin.readline
+                
+def solution():
+    print("\\    /\\")
+    print(" )  ( ')")
+    print("(  /  )")
+    print(" \\(__)|")
+    
+if __name__ == '__main__':
+    solution()
