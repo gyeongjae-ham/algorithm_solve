a2beeb2a53f06b3a66b7b574f3db5f4873313e24
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-02-01 00:07:17 +0900

    [Bronze II] Title: 방 배정, Time: 60 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -3,19 +3,18 @@ fi = sys.stdin.readline
                 
 def solution():
     n,k = map(int, fi().split())
-    wl = [0] * 6
-    ml = [0] * 6
+    cnt = [[0,0,0,0,0,0],[0,0,0,0,0,0]] 
     for _ in range(n):
         s, g = map(int, fi().split())
-        if s == 0:
-            wl[g-1] += 1
-        else:
-            ml[g-1] += 1
+        cnt[s][g-1] += 1
     
     answer = 0
-    for i in range(len(wl)):
-        answer += wl[i] // 2 + wl[i] % 2
-        answer += ml[i] // 2 + ml[i] % 2
+    for i in cnt:
+        for j in i:
+            if j % k == 0:
+                answer += j // k
+            else:
+                answer += j // k + 1
     print(answer)
     
 if __name__ == '__main__':
