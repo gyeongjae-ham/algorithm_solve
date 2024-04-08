3293cdfb586814f11cd5b0c2e3d448640db26b03
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-04-08 22:09:58 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi cin.tie(0)->sync_with_stdio(0)
+
+int main() {
+    fi;
+    
+    cout<<"\\    /\\"<<"\n";
+    cout<<" )  ( ')"<<"\n";
+    cout<<"(  /  )"<<"\n";
+    cout<<" \\(__)|"<<"\n";
+  
+    return 0;
+}
