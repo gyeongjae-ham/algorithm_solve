d0bb618c4d5ae96709a0801ce4284fe3c8597fbd
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-04-09 12:52:04 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define fi cin.tie(0)->sync_with_stdio(0)
+#define ll long long
+
+using namespace std;
+
+int main() {
+  fi;
+    
+  int n;
+  cin>>n;
+  for(int i=1;i<=n;i++) {
+    for(int j=1;j<=i;j++) cout<<"*";
+    for(int j=2*(n-i);j>0;j--) cout<<" ";
+    for(int j=1;j<=i;j++) cout<<"*";
+    
+    cout<<"\n";
+  } 
+  for(int i=1;i<=n-1;i++) {
+    for(int j=n-i;j>0;j--) cout<<"*";
+    for(int j=1;j<=2*i;j++) cout<<" ";
+    for(int j=n-i;j>0;j--) cout<<"*";
+    
+    cout<<"\n";
+  }
+  
+  return 0;
+}
