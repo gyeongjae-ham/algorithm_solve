8935fc342ead6b3c13593efaff41f1c7b93d82b3
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-06-03 16:37:53 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int al[26];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string s;
+    cin>>s;
+    
+    cout << s.size();
+    
+    return 0;
+}
