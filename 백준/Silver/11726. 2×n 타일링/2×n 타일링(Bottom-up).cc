907548e99748864b4704a0b90101d3248dc8b7a0
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-06-07 14:50:58 +0900

    2*n 타일링 Bottom-up code 추가

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링(Bottom-up).cc
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+using namespace std;
+
+int d[1001];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 가장 작은 계산
+    d[0] = 1;
+    d[1] = 1;
+    int n;
+    cin >> n;
+    for (int i = 2; i <= n; i++) {
+        d[i] = d[i - 1] + d[i - 2];
+        d[i] %= 10007;
+    }
+    cout << d[n] << '\n';
+    return 0;
+}
